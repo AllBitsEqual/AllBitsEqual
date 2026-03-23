fefbea70677df990c0d5ed4d39265a94f5945740
AllBitsEqual <allbitsequal@gmail.com>
2026-03-23 01:15:07 +0100

    blog hidden

--- apps/portfolio/app/page.tsx
+++ apps/portfolio/app/page.tsx
@@ -2,7 +2,6 @@ import { Nav } from '@/components/Nav'
 import { Hero } from '@/components/Hero'
 import { SkillsStrip } from '@/components/SkillsStrip'
 import { FeaturedProjects } from '@/components/FeaturedProjects'
-import { BlogTeaser } from '@/components/BlogTeaser'
 import { Footer } from '@/components/Footer'
 
 export default function Home() {
@@ -13,7 +12,6 @@ export default function Home() {
         <Hero />
         <SkillsStrip />
         <FeaturedProjects />
-        <BlogTeaser />
       </main>
       <Footer />
     </>
